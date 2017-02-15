50d01eec64b0aac64310e33dbc6336f1abed7c43
Michael Disko <disko@art-lemon.com>
2017-02-15 23:16:30 +0200

    tabs

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -6,9 +6,20 @@ $(() => {
   navigation();
   $('#section-1').zellionTabs();
   $('#section-2').zellionTabs({
-    defaultTab: 4
+    animation: 'fade'
   });
   $('#section-3').zellionTabs({
+    animation: 'slide',
+    direction: 'left'
+  });
+  $('#section-4').zellionTabs({
+    animation: 'slide',
+    direction: 'bottom'
+  });
+  $('#section-5').zellionTabs({
+    defaultTab: 4
+  });
+  $('#section-6').zellionTabs({
     autoplay: true
   });
 });
